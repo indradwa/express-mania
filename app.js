e3de78ce458882edc8bc7dde0c061105d5073630
indradwa <indradwwa@gmail.com>
2023-10-06 16:31:51 +0700

    add fake db

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 
+const fs = require('fs')
+
 const app = express()
+
 const PORT = 3000
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    console.log(__dirname)
+    res.sendFile('./about.html', { root: __dirname})
+})
+
+app.get('/users', (req, res) => {
+    fs.readFile('./db/fake_database.json', 'utf-8', (error, data) => {
+        if(error) res.send("terjadi kesalahan pembacaan file")
+        res.send(JSON.parse(data))
+    })
 })
 
 app.listen(PORT, () => {
